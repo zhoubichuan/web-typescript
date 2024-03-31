219176e5acaba619fbe677edf81f56e628e2532e
zhoubichuan <zhoubichuan@icloud.com>
2024-03-31 15:04:40 +0800

    更新文档

--- src/.vuepress/nav.js
+++ src/.vuepress/nav.js
@@ -1,23 +1,24 @@
 const nav = [
   {
     text: '基础知识',
-    items: [{
-      text: '一.基本练习',
-      link: '/base/build/1.init'
-    },
-    {
-      text: '二.相关实践',
-      link: '/base/practice/1.docker',
-    }
+    items: [
+      {
+        text: '一.TypeScript',
+        link: '/base/1.typescript/1.index'
+      },
+      {
+        text: '二.Vue相关',
+        link: '/base/2.vue/1.index'
+      },
+      {
+        text: '三.React相关',
+        link: '/base/3.react/1.index',
+      }
     ]
   },
   {
     text: '高级知识',
     items: [
-      {
-        text: '一.TypeScript',
-        link: '/senior/2.typescript/1.index'
-      },
       {
         text: '二.Vue项目',
         link: '/senior/use/1.recursionAndDynamics'
--- src/.vuepress/sidebar.js
+++ src/.vuepress/sidebar.js
@@ -1,30 +1,5 @@
 const sidebar = {
-  '/base/build/': [
-    '1.init',
-    '1.config',
-    '2.webpack',
-    '3.file',
-    '4.single',
-    '5.page',
-    '7.module',
-    '8.project',
-    '9.utils',
-    '10.ui',
-    '11.data',
-    '12.skill',
-    '13.com',
-    '14.data',
-  ],
-  '/base/practice/': [
-    '1.docker', 
-    '2.Jenkins', 
-    '3.gitlab', 
-    'vscode', 
-    'test', 
-    'prem', 
-    'i18n'
-  ],
-  '/senior/2.typescript/': [
+  '/base/1.typescript/': [
     '1.index',
     '1.env',
     '2.base',
@@ -45,22 +20,48 @@ const sidebar = {
     '17.module',
     '18.statement',
     '19.extend'
-],
+  ],
+  '/base/2.vue/': [
+    '1.index',
+    '1.config',
+    '2.webpack',
+    '3.file',
+    '4.single',
+    '5.page',
+    '7.module',
+    '8.project',
+    '9.utils',
+    '10.ui',
+    '11.data',
+    '12.skill',
+    '13.com',
+    '14.data',
+  ],
+  '/base/3.react/': [
+    '1.index',
+    '2.Jenkins',
+    '3.gitlab',
+    'vscode',
+    'test',
+    'prem',
+    'i18n'
+  ],
+
   '/senior/use/': [
-    '1.recursionAndDynamics', 
-    '2.extend', 
-    '3.componentCommunication', 
-    '4.render', 
-    '5.api', 
-    '6.form', 
-    '7.checkBox', 
-    '8.active', 
-    '9.alert', 
-    '10.table', 
+    '1.recursionAndDynamics',
+    '2.extend',
+    '3.componentCommunication',
+    '4.render',
+    '5.api',
+    '6.form',
+    '7.checkBox',
+    '8.active',
+    '9.alert',
+    '10.table',
     '11.tree'
   ],
   '/senior/npm/': [
-    
+
   ]
 };
 module.exports = sidebar;
