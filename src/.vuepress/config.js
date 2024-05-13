3e276e327cd0dfa3dabd432a72f625cf4d4cfd76
周必川 <zhoubichuan@icloud.com>
2024-05-13 21:48:12 +0800

    更新文档

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,19 +1,42 @@
 const path = require("path");
+const httpRequest = require("./public/mock/http.js");
+const Webpack = require("webpack");
+const bodyParser = require("body-parser");
 
 module.exports = {
   title: "TypeScript学习笔记",
   description: "风浪没平息 我宣告奔跑的意义",
-  base: "/web-typescript/", // 部署站点的基础路径
+  base: "/web-typescript/", // 部署站点的基础路径署站点的基础路径
   port: 3009,
   head: [
+    ['link', { rel: 'icon', href: '/home.svg' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
     ["script", { src: "/dll/vendor.dll.js" }],
+    [
+      "script",
+      {
+        src: "https://webapi.amap.com/maps?v=2.0&key=46c9ed4e2d25a0e0ee7c883fd5b1a0c8",
+      },
+    ],
+    [
+      "script",
+      {
+        src: "https://webapi.amap.com/ui/1.1/main.js?v=1.1.1",
+      },
+    ],
   ],
   define: {
     env: {
       NODE_ENV: process.env.NODE_ENV,
     },
   },
+  beforeDevServer(app, server, compiler) {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    httpRequest(app);
+  },
   alias: {
+    mock: path.resolve(__dirname, "../../mock/"),
     "@": path.resolve(__dirname, "../../src/"),
   },
   postcss: {
@@ -22,15 +45,110 @@ module.exports = {
   stylus: { preferPathResolver: "webpack" },
   less: {},
   scss: {
-
+    data: `
+    @import "~@/assets/style/var.scss";
+    @import "~@/assets/style/variables.scss";
+    @import "~@/assets/style/reset.scss";
+    @import "~@/assets/style/mixins.scss";
+    `,
   },
+  sass: { indentedSyntax: true },
   dest: "web-typescript", // 指定 vuepress 的输出目录
   markdown: {
     toc: { includeLevel: [2, 3] },
     lineNumbers: true, // 代码块显示行号
   },
   plugins: [
-
+    // 设置环境变量
+    new Webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: "'production'",
+        BASE_API: "'/'",
+      },
+    }),
+    new Webpack.DllReferencePlugin({
+      manifest: require(path.resolve(
+        __dirname,
+        "public/dll/vendor-manifest.json"
+      )),
+      name: "[name]_[hash]",
+      context: process.cwd(),
+    }),
+    [require("./demo-preview")],
+    [
+      "vuepress-plugin-anchor-toc",
+      {
+        showDepth: 1,
+        customClass: "your-customClass",
+        ignore: ["/", "/api/"],
+      },
+    ],
+    ['fulltext-search'],
+    // 只要把这个放进 config的plugins中就可以了
+    [
+      "sakura",
+      {
+        num: 20, // 默认数量
+        show: true, //  是否显示
+        zIndex: -1, // 层级
+        img: {
+          replace: false, // false 默认图 true 换图 需要填写httpUrl地址
+          httpUrl: "...", // 绝对路径
+        },
+      },
+    ],
+    [
+      // 名称：@vuepress/plugin-pwa 网页内容有更新的时候有刷新按钮。可以把网页保存到桌面，当一个app一样
+      "@vuepress/pwa",
+      {
+        serviceWorker: true,
+        updatePopup: {
+          message: "有新的内容更新",
+          buttonText: "刷新",
+        },
+      },
+    ],
+    [
+      "copyright",
+      {
+        authorName: "zhoubichuan@icloud.com", // 选中的文字将无法被复制
+        minLength: 30, // 如果长度超过  30 个字符
+      },
+    ],
+    // [
+    //   "vuepress-plugin-nuggets-style-copy",
+    //   {
+    //     copyText: "复制代码",
+    //     tip: {
+    //       content: "复制成功",
+    //     },
+    //   },
+    // ],
+    ["@vuepress/back-to-top"],
+    ["@vuepress/medium-zoom"],
+    ["@vuepress/nprogress"],
+    [
+      // 名称：@vuepress/plugin-active-header-links 效果：页面滚动时自动激活侧边栏链接的插件，效果就是右边内容滚动的时候，看到哪里了，左侧菜单会自动高亮显示当前看的目录。
+      "@vuepress/active-header-links",
+      {
+        sidebarLinkSelector: ".sidebar-link",
+        headerAnchorSelector: ".header-anchor",
+      },
+    ],
+    [
+      "@vssue/vuepress-plugin-vssue",
+      {
+        platform: "github", //v3的platform是github，v4的是github-v4
+        locale: "zh", //语言
+        // 其他的 Vssue 配置
+        owner: "zhoubichuan", //github账户名
+        repo: "web-vue2x", //github一个项目的名称
+        clientId: "Iv1.2923ba5d4de48a3c", //注册的Client ID
+        clientSecret: "110210", //注册的Client Secret
+        autoCreateIssue: true, // 自动创建评论，默认是false，最好开启，这样首次进入页面的时候就不用去点击创建评论的按钮了
+      },
+    ],
+    ["vuepress-plugin-boxx"],
   ],
   // 主题配置
   themeConfig: {
--- src/.vuepress/nav.js
+++ src/.vuepress/nav.js
@@ -3,15 +3,15 @@ const nav = [
     text: '基础知识',
     items: [
       {
-        text: '一.TypeScript',
+        text: '一.ts基础',
         link: '/base/1.typescript/1.index'
       },
       {
-        text: '二.Vue相关',
+        text: '二.vue配置',
         link: '/base/2.vue/1.index'
       },
       {
-        text: '三.React相关',
+        text: '三.react配置',
         link: '/base/3.react/1.index',
       }
     ]
@@ -20,11 +20,11 @@ const nav = [
     text: '高级知识',
     items: [
       {
-        text: '二.Vue项目',
+        text: '一.Vue项目',
         link: '/senior/use/1.index'
       },
       {
-        text: '三.React项目',
+        text: '二.React项目',
         link: '/senior/npm/1.index'
       },
     ]
