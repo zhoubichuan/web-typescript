2cc66724211b423631e43ca55ffde8f112bf9fcf
周必川 <zhoubichuan@icloud.com>
2025-08-14 00:12:25 +0800

    更新

---
+++ src/base/2.config/jsToTs/built/1.js
@@ -0,0 +1,12 @@
+"use strict";
+// 确定类型1
+var arr1 = [1, 2, 3];
+var arr2 = ["1", "2", "3"];
+var arr3 = [1, "2", 3];
+// 确定类型2
+var arr = [1, 2, 3];
+var arr4 = [1, "2", 3];
+var arr5 = [1, 2, 3];
+var arr6 = [1, "2", 3];
+var node = [{ id: '112121', children: [] }];
+var node2 = [{ id: '112121', children: [{ id: '112121', children: [] }] }];
---
+++ src/base/2.config/jsToTs/src/1.js
@@ -0,0 +1,12 @@
+"use strict";
+// 确定类型1
+let arr1 = [1, 2, 3];
+let arr2 = ["1", "2", "3"];
+let arr3 = [1, "2", 3];
+// 确定类型2
+let arr = [1, 2, 3];
+let arr4 = [1, "2", 3];
+let arr5 = [1, 2, 3];
+let arr6 = [1, "2", 3];
+const node = [{ id: '112121', children: [] }];
+const node2 = [{ id: '112121', children: [{ id: '112121', children: [] }] }];
